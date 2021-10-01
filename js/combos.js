c961df61d7f98c1626a52ffdc9e0e4f3b9fb12b0
Kenie4r <54483710+Kenie4r@users.noreply.github.com>
2021-10-01 10:27:49 -0600

    A

--- js/combos.js
+++ js/combos.js
@@ -41,6 +41,22 @@ function Inicio(){
 
     //PARTE: CAJA DE SUGERENCIAS   ------------------------------------------
    
+    //PARTE DE MENU
+
+
+    const request = new XMLHttpRequest();
+    
+    //pedimos abrir el JSON con los menus
+    request.open('GET', '../db/combos.json');
+    //El tipo de datos que  vamos a obtener
+    request.responseType = 'json';
+    request.send();
+    //función en la cual nos vamos a guiar cuando se termine de leer el JSON
+    request.onload =  function() {
+        const menu = request.response;
+        escogerMenu(menu);
+    }
+   
 }
 
 function escogerMenu(menu){
@@ -111,3 +127,52 @@ function hiddenTxt() {
     }
 }
 //FUNCIONES: CAJA DE SUGERENCIAS  --------------------------------------------
+
+//FUNCIONES DEL MENU
+
+
+function escogerMenu(menu){
+    var inputFinal = document.getElementById('txtFinal');
+    var menucart = document.getElementById('menu');
+    document.getElementsByName('combos').forEach(e=>{
+        e.addEventListener('click', function(){
+            //menucart.innerHTML = "";
+            var div = document.createElement('div')
+            div.className = "comida"
+            var nombre = document.createElement('div')
+            var a ;
+            var precio = menu[e.id]['precio'][0];
+            switch(e.id){
+                case "comboInfantil":
+                    a = "Combo Infantil"
+                break;
+                case "comboPersonal":
+                    a = "Combo Personal"
+
+                break;
+                case "superCombo":
+                    a = "Super Combo"
+                break;
+            }
+            nombre.innerHTML += `<p class='name'>${a} </p>`
+          
+            div.appendChild(nombre);
+           var menuIn = menu[e.id]['ingredientes']
+            for(var index = 0 ; index <= 4; index++){
+                if(menuIn[index] == undefined){
+
+                }else{
+                    div.innerHTML += `<p>${menuIn[index]}</p>`
+
+                }
+            }
+            var cuentaactual = inputFinal.value;
+            var cuenta = parseFloat(cuentaactual)+parseFloat(precio).toFixed(2)
+            inputFinal.value = cuenta;
+            menucart.appendChild(div);
+
+
+
+        })
+    })
+}
